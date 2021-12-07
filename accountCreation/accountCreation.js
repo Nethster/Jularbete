4d16dbf4f139efaf0cfce05a2d5f1cdb88f731a4
unknown <tom.eriksson88@gmail.com>
2021-12-07 23:04:37 +0100

    modal with function done

--- accountCreation/accountCreation.js
+++ accountCreation/accountCreation.js
@@ -1,20 +1,35 @@
-//Modal for signup
-var signUpModal = document.querySelector("signupModal");
-//Modal signup button
-var signBtn = document.querySelector(".signBtn");
-//open the modal
-signBtn.onclick = function () {
-	modal();
-};
-//close modal
-const spanClose = document.querySelector(".close");
-//close click
-spanClose.onclick = function () {
-	modal.hide();
-};
-//close by clicking outside of the window
-window.onclick = function (b) {
-	if (b.target == signupModal) {
-		signupModal.hide();
+//get modal element
+
+var modal = document.querySelector("#signUpModal");
+
+//get open modal button
+var modalBtn = document.querySelector("#modalBtn");
+
+// close button
+
+var closeBtn = document.querySelector(".closeBtn");
+
+//listen for open click
+modalBtn.addEventListener("click", openModal);
+
+//listen for close click
+closeBtn.addEventListener("click", closeModal);
+
+//Listen for outside click to close
+window.addEventListener("click", clickOutside);
+
+//function to open modal
+function openModal() {
+	modal.style.display = "block";
+}
+
+//function to close modal
+function closeModal() {
+	modal.style.display = "none";
+}
+//clicking outside of the modal closes the modal
+function clickOutside(e) {
+	if (e.target == modal) {
+		modal.style.display = "none";
 	}
-};
+}

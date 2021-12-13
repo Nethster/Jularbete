ae234e8f5c76b70acaa0935932573a69ef1f1786
unknown <tom.eriksson88@gmail.com>
2021-12-13 16:14:31 +0100

    changed the dark theme to the animation and a smoother transformation that's easier on the eyes, just changed the html and CSS slightly

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,10 @@
 const themeSwitcher = () => {
     document.getElementsByTagName('html')[0].className = document.getElementsByTagName('html')[0].className === 'light' ? 'dark' : 'light'
 }
+
+const toggle = document.getElementById("chk");
+
+toggle.addEventListener("change", () => {
+	//theme change
+	document.body.classList.toggle("dark");
+});

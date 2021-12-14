949686d5d4217240ed59f4a959cf3d267249dff3
unknown <tom.eriksson88@gmail.com>
2021-12-14 16:58:57 +0100

    localstorage for darkmode set, now works over all pages as long as JS is connected

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,41 @@
 const themeSwitcher = () => {
-    document.getElementsByTagName('html')[0].className = document.getElementsByTagName('html')[0].className === 'light' ? 'dark' : 'light'
+	document.getElementsByTagName("html")[0].className =
+		document.getElementsByTagName("html")[0].className === "light"
+			? "dark"
+			: "light";
+};
+
+//setting up local storage for darktheme to follow through several pages
+let darkMode = localStorage.getItem("darkMode");
+
+// toggle for the dark mode
+const toggle = document.querySelector("#chk");
+
+// All selectors and functions for the full effect across all pages by storing it.
+const enableDarkMode = () => {
+	document.body.classList.toggle("dark");
+	document.body.classList.add("darkMode");
+	localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+	document.body.classList.toggle("dark");
+	document.body.classList.remove("darkMode");
+	localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+	enableDarkMode();
 }
 
-const toggle = document.getElementById("chk");
-
 toggle.addEventListener("change", () => {
-	//theme change
-	document.body.classList.toggle("dark");
+	darkMode = localStorage.getItem("darkMode");
+
+	if (darkMode !== "enabled") {
+		enableDarkMode();
+		console.log(darkMode);
+	} else {
+		disableDarkMode();
+		console.log(darkMode);
+	}
 });

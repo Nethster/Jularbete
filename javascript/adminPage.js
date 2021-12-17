4c7669516589bd1ba34bdc6374f9eb7090900309
Erik Dalgrund <erikdalgrund91@gmail.com>
2021-12-17 13:20:29 +0100

    fuck my life

--- javascript/adminPage.js
+++ javascript/adminPage.js
@@ -17,7 +17,7 @@ function renderProducts() {
             <img src=./images/${product.img} class="product__image" alt="">
             <h3>${product.name}</h3>
             <h4>${product.price} SEK</h4>
-            <button class="add-to-favorites" title="Add to favorites">
+            <button onclick="addToWishlist()" class="add-to-favorites" title="Add to favorites">
                 <img src="images/favorite_border_black_24dp.svg" alt="">
             </button>
             <button class="add-to-cart" title="Add to Shopping Cart">
@@ -32,13 +32,14 @@ function renderProducts() {
 } 
 
 function addProduct() {
-    const name = document.querySelector("#name").value;
-    const price = document.querySelector("#price").value;
-    const img = document.querySelector("#chooseFile").files[0].name;
+    let name = document.querySelector("#name").value;
+    let price = document.querySelector("#price").value;
+    let img = document.querySelector("#chooseFile").files[0].name;
       let newItem = {
-        name,
-        price,
-        img,
+        name: name,
+        price: price,
+        img: img,
+        addtowish: false
     }
     
     productList.push(newItem);
@@ -55,4 +56,25 @@ function loadProducts() {
 
     return null
 } 
+
+renderProducts() 
+
+let addButton = document.querySelectorAll(".add-to-favorites"); 
+
+for (let i = 0; i < addButton.length; i++); {
+    console.log(addButton) 
+    
+}
+    
+function addToWishlist(namn) {
+    var wishlist = JSON.parse(localStorage.getItem("keta")); 
+    for (let i = 0; i < wishlist.length; i++) {
+        if (wishlist[i].name == namn) {
+            wishlist[i].addtowish = true
+        }
+    } 
+    
+    localStorage.keta = JSON.stringify(wishlist)
+}
+
 
--- javascript/products-database.js
+++ javascript/products-database.js
@@ -2,31 +2,37 @@ let productList = [ 
     {
         name: "hej1", 
         price: 50, 
-        img: "pot-1.jpg"
+        img: "pot-1.jpg",
+        addtowish: false
 },
 {
     name: "hej2", 
     price: 50, 
-    img: "pot-2.jpg"
+    img: "pot-2.jpg", 
+    addtowish: false
 },
 {
     name: "hej3", 
     price: 50, 
-    img: "pot-3.jpg"
+    img: "pot-3.jpg", 
+    addtowish: false
 },
 {
     name: "hej4", 
     price: 50, 
-    img: "pot-4.jpg"
+    img: "pot-4.jpg", 
+    addtowish: false
 },
 {
     name: "hej5", 
     price: 50, 
-    img: "pot-5.jpg"
+    img: "pot-5.jpg", 
+    addtowish: false
 },
 {
     name: "hej6", 
     price: 50, 
-    img: "pot-6.jpg"
+    img: "pot-6.jpg", 
+    addtowish: false
 },
 ]

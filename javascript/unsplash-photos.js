d0d5c8b4402e01c491053e5afe8119a568b4fe34
Liudmila Kazakbaeva <83576552+liudmilitas@users.noreply.github.com>
2021-12-17 09:52:35 +0100

    new quote function

--- javascript/unsplash-photos.js
+++ javascript/unsplash-photos.js
@@ -11,6 +11,8 @@ async function addImage () {
   // awaiting random image's url from the getNewImage()
   let randomImage = await getNewImage();
   unsplashImage.src = randomImage;
+  let randomQuote = newQuote();
+  quote.innerHTML = randomQuote;
 };
 
 //fetching a random image from Unsplash API, returns a url
@@ -24,6 +26,26 @@ async function getNewImage() {
   });
 }
 
+const newQuote = () => {
+  let randomNumber = Math.floor(Math.random() * plantQuotes.length);
+  return plantQuotes[randomNumber];
+  console.log(plantQuotes[randomNumber]);
+}
+
+const plantQuotes = [
+  "A beautiful plant is like having a friend around the house. Beth Ditt",
+  "Even if I knew that tomorrow the world would go to pieces, I would still plant my apple tree. Martin Luther",
+  "Plants are solar powered air purifiers whose filter never needs replacing. Khang Kijarro Nguyen",
+  "Plants do not speak, but their silence is alive with change. May Sarton",
+  "To nurture a garden is to feed not just the body, but the soul. Alfred Austin",
+  "All life on earth emanates from the green of the plant.",
+  "I'm really quite simple. I plant flowers and watch them grow... I stay at home and watch the river flow. George Harrison",
+  "The best time to plant a tree was 20 years ago. The second best time is now. Chinese proverb",
+  "One touch of nature makes the whole world kin. William Shakespeare",
+  "If you've never experienced the joy of accomplishing more than you can imagine, plant a garden. Robert Brault."
+]
+
+/*
 //fetching a random quote from type.fit API
 async function getNewQuote() {
   let randomIndex = Math.floor(Math.random()*allQuotes.length);
@@ -38,5 +60,5 @@ async function getNewQuote() {
     quote.innerHTML = qt;
     author.innerHTML = auth;
   });
-
 }
+*/

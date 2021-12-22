39cb01ff191362bcb16305cfb37352297703a43f
unknown <tom.eriksson88@gmail.com>
2021-12-22 15:38:24 +0100

    roll feature active, still bugs though, working it out!

--- javascript/account-creation.js
+++ javascript/account-creation.js
@@ -7,8 +7,6 @@ var modalBtn = document.querySelector("#modalBtn");
 // close button
 var closeBtn = document.querySelector(".closeBtn");
 
-//modal for F.A.Q
-
 //listen for open click
 modalBtn.addEventListener("click", openModal);
 
@@ -20,53 +18,53 @@ window.addEventListener("click", clickOutside);
 
 //function to open modal
 function openModal() {
-  modal.style.display = "block";
+	modal.style.display = "block";
 }
 
 //function to close modal
 function closeModal() {
-  modal.style.display = "none";
+	modal.style.display = "none";
 }
 //clicking outside of the modal closes the modal
 function clickOutside(e) {
-  if (e.target == modal) {
-    modal.style.display = "none";
-  }
+	if (e.target == modal) {
+		modal.style.display = "none";
+	}
 }
 
 //Localstorage for member registration
 
 function saveData() {
-  let user, psw, email;
-  user = document.querySelector("#userID").value;
-  psw = document.querySelector("#passwordID").value;
-  email = document.querySelector("#emailID").value;
+	let user, psw, email;
+	user = document.querySelector("#userID").value;
+	psw = document.querySelector("#passwordID").value;
+	email = document.querySelector("#emailID").value;
 
-  let memberRecords = new Array();
+	let memberRecords = new Array();
 
-  memberRecords = JSON.parse(localStorage.getItem("users"))
-    ? JSON.parse(localStorage.getItem("users"))
-    : [];
-  if (
-    memberRecords.some((x) => {
-      return x.email == email;
-    })
-  ) {
-    alert("Email already in use");
-  } else if (
-    memberRecords.some((z) => {
-      return z.username == user;
-    })
-  ) {
-    alert("Username already in use");
-  } else {
-    memberRecords.push({
-      username: user,
-      password: psw,
-      email: email,
-    });
-    localStorage.setItem("users", JSON.stringify(memberRecords));
-  }
+	memberRecords = JSON.parse(localStorage.getItem("users"))
+		? JSON.parse(localStorage.getItem("users"))
+		: [];
+	if (
+		memberRecords.some((x) => {
+			return x.email == email;
+		})
+	) {
+		alert("Email already in use");
+	} else if (
+		memberRecords.some((z) => {
+			return z.username == user;
+		})
+	) {
+		alert("Username already in use");
+	} else {
+		memberRecords.push({
+			username: user,
+			password: psw,
+			email: email,
+		});
+		localStorage.setItem("users", JSON.stringify(memberRecords));
+	}
 }
 
 /* 
@@ -74,9 +72,27 @@ function saveData() {
 	localStorage.setItem("password", psw);
 	localStorage.setItem("email", email); */
 
-//get modal element
-var modal = document.querySelector(".faqModal");
+//FAQ modal
+
 //get open modal button
-var modalBtn = document.querySelector("#faqModalAct");
-// close button
-var closeBtn = document.querySelector(".closeBtn");
+var modalBtnFAQ = document.querySelector("#rollFAQ");
+
+//get modal element
+var modalFAQ = document.querySelector(".modalFAQ");
+
+//listen for open click
+modalBtnFAQ.addEventListener("click", openModalBtnFAQ);
+
+//Listen for outside click to close
+window.addEventListener("click", clickOutside);
+
+//function to open modal
+function openModalBtnFAQ() {
+	modalFAQ.style.display = "block";
+}
+
+function clickOutside(e) {
+	if (e.target == modalFAQ) {
+		modalFAQ.style.display = "none";
+	}
+}
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -40,3 +40,4 @@ toggle.addEventListener("change", () => {
 	}
 });
 
+
